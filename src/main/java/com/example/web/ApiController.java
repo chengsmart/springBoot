2b26157c8fe15d32a1a1f3400532043249b416e5
Smart <chengshuai.tj@gmail.com>
2019-01-22 15:39:22 +0800

    完成相关配置

--- src/main/java/com/example/web/ApiController.java
+++ src/main/java/com/example/web/ApiController.java
@@ -1,11 +1,10 @@
-package com.example.demo;
+package com.example.web;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ApiController {
-
     @RequestMapping("/test")
     public String index() {
         return "Hello world";
